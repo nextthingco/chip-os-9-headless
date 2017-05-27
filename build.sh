793cd8e71a5c7dfaacda20310b1af89326ca85fa
computermouth <computermouth@crunchbangplusplus.org>
2017-05-26 17:37:51 -0700

    First take

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#if [[ $(grep "tar \${LIVE_IMAGE_NAME}-\${LIVE_IMAGE_ARCHITECTURE}." /usr/lib/live/build/binary_tar) ]]; then
+#        sudo sed -s -i 's%tar ${LIVE_IMAGE_NAME}-${LIVE_IMAGE_ARCHITECTURE}.%tar binary%' /usr/lib/live/build/binary_tar
+#fi
+
+sudo lb build
+rm -rf live-image-armhf.tar.tar
+pushd binary
+sudo tar -cf ../rootfs.tar .
+popd
+
+CHIP_UBOOT_BRANCH=${CHIP_UBOOT_BRANCH:-production-mlc}
+
+git clone https://github.com/nextthingco/chip-u-boot
+pushd chip-u-boot
+
+git checkout ${CHIP_UBOOT_BRANCH}
+
+make ${UBOOT_EXTRA_OPTS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CHIP_defconfig
+make ${UBOOT_EXTRA_OPTS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+
