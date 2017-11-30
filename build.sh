138efb93c6ccafc15a33cb39b04e0d7d0bd8d2a0
computermouth <computermouth@crunchbangplusplus.org>
2017-11-29 16:38:04 -0800

    Clean up to match chip-pro-debian-9

--- build.sh
+++ build.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#if [[ $(grep "tar \${LIVE_IMAGE_NAME}-\${LIVE_IMAGE_ARCHITECTURE}." /usr/lib/live/build/binary_tar) ]]; then
-#        sudo sed -s -i 's%tar ${LIVE_IMAGE_NAME}-${LIVE_IMAGE_ARCHITECTURE}.%tar binary%' /usr/lib/live/build/binary_tar
-#fi
-
 if ! mount | grep binfmt_misc;
 then
 	sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 fi
 
-#sudo sed -i 's|#!/bin/sh|#!/bin/sh -x|g' /usr/lib/live/build/*
-
 sudo lb build
-rm -rf live-image-armhf.tar.tar
+rm -rf live-image-armhf.tar.tar rootfs.tar
 pushd binary
 sudo tar -cf ../rootfs.tar .
 popd
 
-CHIP_UBOOT_BRANCH=${CHIP_UBOOT_BRANCH:-ww/2016.01/next}
+CHIP_UBOOT_BRANCH=${CHIP_UBOOT_BRANCH:-production-mlc}
 
 git clone https://github.com/nextthingco/chip-u-boot
 pushd chip-u-boot
 
 git checkout ${CHIP_UBOOT_BRANCH}
+git apply ../0001-compiler-.h-sync-include-linux-compiler-.h-with-Linu.patch
 
 make ${UBOOT_EXTRA_OPTS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CHIP_defconfig
 make ${UBOOT_EXTRA_OPTS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
 
+## To create image/flash:
+##
+## sudo ./chip-create-nand-image.sh ../chip-pro-debian-9/chip-u-boot ../chip-pro-debian-9/rootfs.tar output
+## sudo chown -R $USER:$USER output
+## ./chip-update-firmware.sh -L output
